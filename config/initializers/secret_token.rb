a3df7a4790dee3edf01768885f3959ab1f8d97b9
Mathieu Gilbert <matnarak@gmail.com>
2012-02-12 21:10:22 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EdibleSystems::Application.config.secret_token = '970e61f2a44ce2670ebe29408cd00929da252606a71e4827319e86a4b7c1b7232880794dda1abe40079f37fd1f05ee54efa08804763e7d7256d22f064bc21323'
