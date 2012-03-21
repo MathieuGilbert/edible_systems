20f702874b60d635de2015ff911e9b2ddc2095d1
Mathieu Gilbert <matnarak@gmail.com>
2012-03-21 17:46:44 -0600

    restyled header, footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,17 @@
 class PagesController < ApplicationController
   def home
+    @current_page = 'home'
   end
 
   def portfolio
+    @current_page = 'portfolio'
   end
 
   def about
+    @current_page = 'about'
   end
 
   def contact
+    @current_page = 'contact'
   end
 end
