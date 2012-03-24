05b1330a4571cc6b8335180b0638e9733f748455
Mathieu Gilbert <matnarak@gmail.com>
2012-03-24 11:15:01 -0600

    configured email

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 EdibleSystems::Application.initialize!
 
+# key for negative captcha form
 NEGATIVE_CAPTCHA_SECRET = '8bdb18772200189130adcfb6a52fb18bb4d8de6515224a928fd05614ed3d5ee90e0e9654e07b3ced3770395e6a91d1cefc6cbd935002558bea3e0ba7d1a37b03'
